6836eee2318509c9b86b78de70b1811da2230abd
marcamvi <marta.cambero@gmail.com>
2022-05-12 11:17:50 +0200

    modify name on models, testing tinker

--- app/Models/entidade.php
+++ app/Models/entidade.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class entidad extends Model
+class entidade extends Model
 {
     use HasFactory;
 
